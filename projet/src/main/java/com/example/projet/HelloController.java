88aa7a1772886eb29827a8039b0f04156c575ad6
ZIANI Ahmed <ahmed.ziani@telecomnancy.eu>
2021-12-13 21:10:36 +0000

    Update HelloController.java

--- projet/src/main/java/com/example/projet/HelloController.java
+++ projet/src/main/java/com/example/projet/HelloController.java
@@ -1,14 +1,82 @@
-package com.example.projet;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+//import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class HelloController   {
+
+    Connection connection = null;
+    PreparedStatement pst;
+    ResultSet rs;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField login;
+    @FXML
+    private Button ok;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    protected void login() {
+
+        String uname = login.getText();
+        String pass = password.getText();
+
+        if (uname.equals("") && pass.equals("")){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Champs vides ");
+            alert.showAndWait();
+            login.setText("");
+            password.setText("");
+            login.requestFocus();
+        }
+
+             try{
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection( "jdbc:sqlite:database.db" );
+                pst = connection.prepareStatement("select * from users where login=(?) and password=(?)");
+                pst.setString(1,uname);
+                pst.setString(2,pass);
+
+                rs = pst.executeQuery();
+
+                if (rs.next()) {
+
+                    System.out.println("found it");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setHeaderText("Utilisateur trouvé! ");
+                    alert.showAndWait();
+
+
+                }
+                else {
+
+                    System.out.println("not exist");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setHeaderText("Utilisateur non trouvé ");
+                    alert.showAndWait();
+                    login.setText("");
+                    password.setText("");
+                    login.requestFocus();
+                }
+            }
+            catch (Exception e){
+                System.out.println(""+e.getMessage());
+            } 
+        }
+
+
 }
+
