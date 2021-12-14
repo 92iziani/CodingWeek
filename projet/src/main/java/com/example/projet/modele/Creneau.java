fd3626710b4a17f6126cef37703ed2ee28791bf6
ALLOUCHE Kamal <kamal.allouche@telecomnancy.eu>
2021-12-14 09:30:14 +0000

    Update

---
+++ projet/src/main/java/com/example/projet/modele/Creneau.java
@@ -0,0 +1,30 @@
+package com.example.projet.modele;
+
+public class Creneau {
+    private String date;
+    private String heure;
+
+    public Creneau() {
+    }
+
+    public Creneau(String date, String heure) {
+        this.date = date;
+        this.heure = heure;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHeure() {
+        return heure;
+    }
+
+    public void setHeure(String heure) {
+        this.heure = heure;
+    }
+}
