1d71f6eb6c02091f26435dd692f432702dea1a97
AxelGoarant <AxelGoarant@telecomnancy.eu>
2021-12-15 16:30:33 +0100

    test

---
+++ ProfRDV/src/controller/listerRDVProfControler.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import modele.RDV;
+
+public class listerRDVProfControler {
+
+    @FXML
+    VBox vboxRDVAttente;
+
+    @FXML
+    Button close;
+
+
+    public void addRDVenattente(){
+        VBox v = new VBox();
+        RDV r = new RDV();
+        v.getChildren().addAll(new Label(r.geteId()));
+        this.vboxRDVAttente.getChildren().add(v);
+    }
+
+    public void closeApplication(){
+        Stage stage = (Stage) close.getScene().getWindow();
+        stage.close();
+    }
+}
