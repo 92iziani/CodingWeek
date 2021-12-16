bc058fabbe30aad3f47396c5f3b129864cc500f4
ZIANI Ahmed <ahmed.ziani@telecomnancy.eu>
2021-12-16 09:17:06 +0000

    Update LoginController.java

--- projet/src/main/java/com/example/projet/LoginController.java
+++ projet/src/main/java/com/example/projet/LoginController.java
@@ -11,11 +11,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.ResourceBundle;
@@ -61,18 +64,29 @@ public class LoginController  implements Initializable {
                 rs = pst.executeQuery();
 
                  if (rs.next()) {
+
                     if(rs.getString("Type").equals("Etudiant")){
                         this.user.setEleve(new Eleve(rs.getString("uId"),rs.getString("Nom"),rs.getString("Email")));
-                    } else{
+                        affiche_listrdvetudiant();
+                    } 
+                    else{
                     if(rs.getString("Type").equals("Professeur")){
                         this.user.setProf(new Prof(rs.getString("uId"),rs.getString("Nom"),rs.getString("Email"),null));
+                        affiche_listrdvetudiant();
+
+                    } else {
+
+                            if(rs.getString("Type").equals("root")){
+                                connection.close();
+                                AnchorPane pane;
+                                FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("AdminPagev2.fxml"));
+                                pane = fxmlLoader.load();
+                                MainContext.getChildren().setAll(pane);
+                            }   
                     }
+                    
                     }
-                connection.close();
-                AnchorPane pane;
-                FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("listerdv-2.fxml"));
-                pane = fxmlLoader.load();
-                MainContext.getChildren().setAll(pane);
+                
                  }
 
                  else {
@@ -96,6 +110,16 @@ public class LoginController  implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
         System.out.println(user.getTest());
+    }
+
+
+
+    public void affiche_listrdvetudiant() throws SQLException, IOException{
+                connection.close();
+                AnchorPane pane;
+                FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("listerdv-2.fxml"));
+                pane = fxmlLoader.load();
+                MainContext.getChildren().setAll(pane);
     }
 
 
